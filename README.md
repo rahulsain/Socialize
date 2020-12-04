47e1fc8f0bda7e5ba1e792a105c83c47fea88696
rahulsain3000 <rahulsain3000@users.noreply.github.com>
2020-12-04 18:58:57 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Socialize
+A social media app designed in Firebase technology.
+
+**Socialize**
+
+
+<img src = ".\img\ss2.jpg" width="50%">
+
+
+**Socialize**
+
+
+<img src = ".\img\ss1.jpg" width="50%">
+
+In Developement
---
+++ app/src/main/java/com/example/socialize/SignInActivity.kt
@@ -0,0 +1,65 @@
+package com.example.socialize
+
+import android.content.Intent
+import android.nfc.Tag
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+    private val RC_SIGN_IN: Int = 123
+    private lateinit var googleSignInClient:GoogleSignInClient
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        signInButton.setOnClickListener{
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account =
+                completedTask.getResult(ApiException::class.java)!!
+            Log.d(TAG, "firebaseAuthWithGoogle" + account.id)
+            firebaseWithGoogle(account.idToken!!)
+        } catch (e: ApiException){
+
+            Log.w(TAG,"signInResult:failed codes"+e.statusCode)
+        }
+    }
+
+    private fun firebaseWithGoogle(idToken: String) {
+
+    }
+}
