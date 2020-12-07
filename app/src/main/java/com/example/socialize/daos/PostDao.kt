cae805d3ba20704c16cd6b461b67830c3232dd65
rahulsain3000 <rahulsain3000@users.noreply.github.com>
2020-12-07 19:33:18 +0530

    finished the app that is working with coroutines

--- app/src/main/java/com/example/socialize/daos/PostDao.kt
+++ app/src/main/java/com/example/socialize/daos/PostDao.kt
@@ -14,7 +14,7 @@ import kotlinx.coroutines.tasks.await
 class PostDao {
 
     private val db = FirebaseFirestore.getInstance()
-    private val postCollections = db.collection("posts")
+     val postCollections = db.collection("posts")
     private val auth = Firebase.auth
 
     fun addPost(text: String) {
@@ -29,7 +29,7 @@ class PostDao {
         }
     }
 
-    fun getPostById(postId: String): Task<DocumentSnapshot> {
+    private fun getPostById(postId: String): Task<DocumentSnapshot> {
         return postCollections.document(postId).get()
     }
 
